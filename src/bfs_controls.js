edea3243383b486b73b5f5e20ad340256fb110c8
Steph Meyering <stephane.meyering@gmail.com>
2020-03-26 17:32:57 -0700

    refactor ui functions

--- src/bfs_controls.js
+++ src/bfs_controls.js
@@ -1,28 +1,38 @@
+import BFSTree from "./bfs_tree";
+import { WIDTH, HEIGHT } from ".";
+
 class BFSInput{
-    constructor(configDiv){
-        this.output = configDiv.querySelector('#tree-params');
+    constructor(ctx, configDiv){
+        // save context and get input slider elements + output div
+        this.ctx = ctx;
+        this.startPos = [WIDTH/2, HEIGHT]
         this.branchAngleSlider = configDiv.querySelector("#branch-angle-input");
         this.depthSlider = configDiv.querySelector("#depth-input");
+        this.output = configDiv.querySelector('#tree-params');
+        
+        // set event listeners on sliders so the params display in real-time
+        this.branchAngleSlider.addEventListener('input', () => this.update());
+        this.depthSlider.addEventListener('input', () => this.update());
     }
 
-    displayUserInput (configDiv) {
-        let output = configDiv.querySelector('#tree-params');
-        let branchAngleSlider = configDiv.querySelector("#branch-angle-input");
-        let depthSlider = configDiv.querySelector("#depth-input");
-        let angle = branchAngleSlider.value;
-        let depth = depthSlider.value
-        output.innerHTML = `depth: ${depth} angle: ${angle}`
+    displayUserInput(depth, angle) {
+        this.output.innerHTML = `depth: ${depth} angle: ${angle} nodes generated on each render: ${2** depth}`
     }
 
-    // branchAngleSlider.oninput = () => {
-    //     displayUserInput();
-    //     calculate();
-    // };
-    // depthSlider.oninput = () => {
-    //     displayUserInput();
-    //     calculate();
-    // } 
+    update(){
+        // get user input values
+        let angle = this.branchAngleSlider.valueAsNumber;
+        let depth = this.depthSlider.valueAsNumber;
+        this.displayUserInput(depth, angle);
+        this.calculate(depth, angle)
+        
+    }
 
+    
+    calculate (depth, angle) {
+        this.ctx.clearRect(0 ,0 , WIDTH, HEIGHT)
+        new BFSTree(this.ctx, this.startPos, depth, angle)
+    }
 }
 
 export default BFSInput;
--- src/index.js
+++ src/index.js
@@ -4,46 +4,29 @@ import BFSInput from "./bfs_controls";
 
 document.addEventListener("DOMContentLoaded", function () {
     const canvasEl = document.getElementById("main-canvas")
+    const ctx = canvasEl.getContext("2d")
     canvasEl.height = window.innerHeight;
     canvasEl.width = window.innerWidth;
     
     let configDiv = document.getElementById('tree-config');
 
-    // let output = document.getElementById('tree-params');
+    let input = new BFSInput(ctx, configDiv)
+
+
+    // let startX = WIDTH/2;
+    // let startY = HEIGHT;
+    // canvasEl.addEventListener("click", e => {
+    //     startX = e.clientX;
+    //     startY = e.clientY + 100;
+    //     calculate()
+    // })
     
-    // let branchAngleSlider = document.getElementById("branch-angle-input");
-    // let depthSlider = document.getElementById("depth-input");
-    
-    // const displayUserInput =() => {
-    //     let angle = branchAngleSlider.value;
-    //     let depth = depthSlider.value
-    //     output.innerHTML = `depth: ${depth} angle: ${angle}`
+    // const calculate = (x=startX, y=startY) => {
+    //     c.clearRect(0, 0, WIDTH, HEIGHT)
+    //     let angle = branchAngleSlider.valueAsNumber
+    //     let depth = depthSlider.valueAsNumber
+    //     new BFSTree(c, [x, y], depth, angle);
     // }
-
-    branchAngleSlider.oninput = () => {
-        displayUserInput();
-        calculate();
-    }
-    depthSlider.oninput = () => {
-        displayUserInput();
-        calculate();
-    } 
-
-    let startX = WIDTH/2;
-    let startY = HEIGHT;
-    canvasEl.addEventListener("click", e => {
-        startX = e.clientX;
-        startY = e.clientY + 100;
-        calculate()
-    })
-    
-    const calculate = (x=startX, y=startY) => {
-        c.clearRect(0, 0, WIDTH, HEIGHT)
-        let angle = branchAngleSlider.valueAsNumber
-        let depth = depthSlider.valueAsNumber
-        new BFSTree(c, [x, y], depth, angle);
-    }
-    const c = canvasEl.getContext("2d")
     
 })
 
